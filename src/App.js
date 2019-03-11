4b31d09253db9549c6fd24e9c8ab1aaa9a8a9d9e
Luke Herrmann <herrmannluke@gmail.com>
2019-03-11 16:23:30 -0600

    Completed Basic Obj's

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import List from "./List"
+import GroceryForm from "./GroceryForm"
 import './App.css';
+import Grocery from './Grocery';
 
 class App extends Component {
+  state = {
+    groceries: [
+      {id: 1, name: "Milk", complete: true},
+      {id: 2, name: "Eggs", complete: false},
+      {id: 3, name: "Cheese", complete: false},
+    ]
+  }
+
+  getUniqId = () => {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+   }
+ 
+  addItem = (name) => {
+    const { groceries } = this.state
+    const grocery = {name, id: this.getUniqId, complete: false}
+    this.setState({ groceries: [...groceries, grocery]})
+  }
+
+  handleClick = (id) => {
+    const { groceries } = this.state;
+    this.setState({
+      groceries: groceries.map( grocery => {
+        if (grocery.id === id) {
+          return {
+            ...grocery,
+            complete: !grocery.complete
+          }
+        }
+        return grocery
+      })
+    })
+  }
+
   render() {
+    const { groceries } = this.state
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+      <List name="Grocery List" items={groceries} groceryClick={this.handleClick} />
+      <GroceryForm addItem={this.addItem}/>
       </div>
     );
   }
